53b788e8c7b0acab0f191a3fe3c5ed9b6a5c8090
Terry Bu <burea1124@gmail.com>
2016-12-23 18:15:02 -0500

    Initial Commit

---
+++ Setbacks to Comebacks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Setbacks to Comebacks
+//
+//  Created by Terry Bu on 12/23/16.
+//  Copyright © 2016 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
