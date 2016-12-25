601d51cc9a41e44184316d0880a5c989c333b35d
Terry Bu <burea1124@gmail.com>
2016-12-24 21:28:10 -0500

    Started on basic tab bar controller and building out the first tableView controller at home

---
+++ Setbacks to Comebacks/HomeTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  HomeTableViewController.swift
+//  Setbacks to Comebacks
+//
+//  Created by Terry Bu on 12/24/16.
+//  Copyright © 2016 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    var peopleArray: [Person]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        peopleArray = [Person]()
+        let newton = Person(name: "Isaac Newton", setbacks: ["Bipolar", "Depression"])
+        let jesus = Person(name: "Jesus Christ", setbacks: ["Suicidal Thoughts", "Betrayal", "Persecution"])
+        let robinWilliams = Person(name: "Robin Williams", setbacks: ["Depression"])
+        peopleArray = [newton, jesus, robinWilliams]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peopleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        let person = peopleArray[indexPath.row]
+        cell.textLabel?.text = person.name
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Setbacks to Comebacks/Person.swift
@@ -0,0 +1,25 @@
+//
+//  Person.swift
+//  Setbacks to Comebacks
+//
+//  Created by Terry Bu on 12/24/16.
+//  Copyright © 2016 Terry Bu. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    
+    var name: String
+    var dob: Date?
+    var biography: String?
+    var occupation: [String]?
+    var setbacks: [String]
+    var placeOfBirth: String?
+    
+    init(name: String, setbacks: [String]) {
+        self.name = name
+        self.setbacks = setbacks
+    }
+    
+}
