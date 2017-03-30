9284307a1f3ae67c1ea9eea79122a55eee705576
Terry Bu <burea1124@gmail.com>
2017-03-30 18:55:29 -0400

    Follow on instagram feature and fixed about app and send email

--- Setbacks to Comebacks/SettingsTableViewController.swift
+++ Setbacks to Comebacks/SettingsTableViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 import MessageUI
 
 enum SettingsCell : Int {
-    case fontSize = 0, aboutThisApp, rateApp, feedback, count
+    case fontSize = 0, aboutThisApp, rateApp, feedback, followDev, count
     
     func label() -> String {
         switch self {
         case .fontSize: return "Set Font Size"
         case .aboutThisApp: return "About This App"
-        case .rateApp: return "Rate this App in App Store "
-        case .feedback: return "Email Feedback to Dev"
+        case .rateApp: return "Rate this App in App Store"
+        case .feedback: return "Email Feedback to Developer"
+        case .followDev: return "Follow Developer on Instagram"
         case .count : return ""
         }
     }
@@ -67,6 +68,12 @@ class SettingsTableViewController: UITableViewController, MFMailComposeViewContr
             navigationController?.pushViewController(fontSize, animated: true)
         } else if cellType == .aboutThisApp {
             //TODO: Prompt an alertview with a brief blurb on what this app is about
+            let aboutAppString = "Setbacks2Comebacks is an educational and motivational iOS app written in Swift. It lists inspiring historical figures who have turned their life obstacles and setbacks into comebacks and tests into testimonies. The writing is a mixture of both copied articles from the Internet and my own. This app was made as a compilation of autobiographies of both past greats and living greats who made significant impact on the world. My sources come from Wikipedia, Google, biography.com and the like. This app will never have advertisements and will always be free. I hope you enjoy it! Please feel free to send me feedback via email or follow me at @burownrice."
+            let alertView = UIAlertController(title: "About This App", message: aboutAppString, preferredStyle: UIAlertControllerStyle.alert)
+            alertView.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in
+                alertView.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alertView, animated: true)
         } else if cellType == .rateApp {
             if let path = URL(string: APP_URL_STRING) {
                 UIApplication.shared.open(path) {
@@ -79,16 +86,20 @@ class SettingsTableViewController: UITableViewController, MFMailComposeViewContr
         } else if cellType == .feedback {
             // Present the view controller modally.
             composeVC = MFMailComposeViewController()
-            composeVC!.mailComposeDelegate = self
-            composeVC!.setToRecipients(["jupiterouroboros@gmail.com"])
-            composeVC!.setSubject("User Feedback from Setbacks 2 Comebacks!!")
-            composeVC!.setMessageBody("Hi, My feedback for your app is ... ", isHTML: false)
-            if !MFMailComposeViewController.canSendMail() {
-                print("Mail services are not available")
-                return
-            } else {
-                self.present(composeVC!, animated: true, completion: nil)
+            if let vc = composeVC {
+                vc.mailComposeDelegate = self
+                vc.setToRecipients(["jupiterouroboros@gmail.com"])
+                vc.setSubject("User Feedback from Setbacks 2 Comebacks!!")
+                vc.setMessageBody("", isHTML: false)
+                if !MFMailComposeViewController.canSendMail() {
+                    print("Mail services are not available")
+                    return
+                } else {
+                    self.present(composeVC!, animated: true, completion: nil)
+                }
             }
+        } else if cellType == .followDev {
+            UIApplication.shared.open(NSURL(string: "http://instagram.com/burownrice")! as URL, options: [:], completionHandler: nil)
         }
     }
     
